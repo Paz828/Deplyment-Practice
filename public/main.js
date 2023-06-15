78202ca828d0fa9aaf83caa36d808638c2a1c871
Dorion Boesch <dorion.boesch@gmail.com>
2023-06-15 13:21:55 -0500

    added some button functionality

--- public/main.js
+++ public/main.js
@@ -1,8 +1,11 @@
-// const btn = document.querySelector('#get-btn');
-// btn.addEventListener('click', async () => {
-//     try {
-//         const response = await fetch()
-//     } catch (err) {
-        
-//     }
-// })
+const btn = document.querySelector('#get-btn');
+btn.addEventListener('click', async () => {
+    try {
+        const response = await fetch('https://wishlist-deploy-practice.onrender.com/wish');
+        const div = document.createElement('div');
+        div.textContent = response
+        document.body.appendChild(div)
+    } catch (err) {
+        console.log(err)
+    }
+})
