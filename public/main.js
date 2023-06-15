4c1dc17c9b69eeda15e227f5cb956d34f7febe7b
Dorion Boesch <dorion.boesch@gmail.com>
2023-06-15 13:51:02 -0500

    hopefully fixed button mk2

--- public/main.js
+++ public/main.js
@@ -3,9 +3,9 @@ btn.addEventListener('click', async () => {
     try {
         const response = await fetch('https://wishlist-deploy-practice.onrender.com/wish');
         const div = document.createElement('div');
-        div.textContent = JSON.parse(response)
-        document.body.appendChild(div)
+        console.log(response);
+        document.body.appendChild(div);
     } catch (err) {
-        console.log(err)
+        console.log(err);
     }
 })
